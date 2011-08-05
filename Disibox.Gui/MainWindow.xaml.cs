20a3922d719154231ba8f42c64313c1c8fccbca3
tartur88 <tartur88@7bd0ccd3-fe19-b6ab-805e-a957cd640a58>
2011-08-05 21:04:32 +0000

    interfaccia... con utente

--- Disibox.Gui/MainWindow.xaml.cs
+++ Disibox.Gui/MainWindow.xaml.cs
@@ -19,10 +19,23 @@ namespace Disibox.Gui {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        private string _user;
+        private DataSource _dataSource;
         public MainWindow() {
             InitializeComponent();
         }
 
+        public string User {
+            get { return _user; }
+            set { _user = value;
+                this.Title = "Disibox - " + _user;
+            }
+        }
+
+        public DataSource Datasource {
+            get { return _dataSource; }
+            set { _dataSource = value; }
+        }
 
         private void button1_Click(object sender, RoutedEventArgs e) {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -31,15 +44,15 @@ namespace Disibox.Gui {
 
             if (result == true) {
                 string filename = ofd.FileName;
-                textBox1.Text = filename;
+                textBoxFileToUpload.Text = filename;
             }
         }
 
         private void button2_Click(object sender, RoutedEventArgs e) {
             var ds = new DataSource();
 
-            if (textBox1.Text != "") {
-                var retn = ds.AddFile(textBox1.Text);
+            if (textBoxFileToUpload.Text != "") {
+                var retn = ds.AddFile(textBoxFileToUpload.Text);
                 MessageBox.Show("The file has been uploaded successfully: " + retn);
             } else {
                 MessageBox.Show("No file to upload");
@@ -55,5 +68,32 @@ namespace Disibox.Gui {
             foreach (var name in names) 
                 listView_Files.Items.Add(name);
         }
+
+        private void ExitProgram(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+
+        private void Logout(object sender, RoutedEventArgs e) {
+            _dataSource.Logout();
+            new LoginWindow().Show();
+            this.Close();
+        }
+
+        private void buttonDeleteFile_Click(object sender, RoutedEventArgs e) {
+            var selectedItem = (FileAndMime)listView_Files.SelectedItem;
+
+            if (selectedItem == null) return;
+            
+            Console.WriteLine(selectedItem.Filename);
+//            to delete the file with this name = selectedItem.Filename
+        }
+
+        private void buttonDownloadFile_Click(object sender, RoutedEventArgs e) {
+            var selectedItem = (FileAndMime)listView_Files.SelectedItem;
+
+            if (selectedItem == null) return;
+//            to download the file with this name = selectedItem.Filename
+        }
+
     }
 }
